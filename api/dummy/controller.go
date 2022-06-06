9dcf904cc36c7a287b941174e814630a485f177b
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-06-07 02:45:03 +0700

    Docs : update

--- api/dummy/controller.go
+++ api/dummy/controller.go
@@ -521,6 +521,12 @@ func (Controller *Controller) InputPoin(c echo.Context) error {
 	data.Amount = req.Amount
 	data.Poin_redeem = price
 
+	for _, v := range Customer {
+		if v.ID == data.Customer_id {
+			v.Poin = v.Poin + i
+		}
+	}
+
 	DetailTransaction = append(DetailTransaction, data)
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
