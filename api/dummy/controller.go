d017ba7301d7349b6e7305ae62e3e46703848290
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-06-08 01:45:16 +0700

    Docs : update

--- api/dummy/controller.go
+++ api/dummy/controller.go
@@ -26,7 +26,6 @@ func NewController(service dummyBussiness.Service) *Controller {
 	}
 }
 
-var Validator *validator.Validate
 var AccountMitra []mitra.Mitra
 var Stockproduct []dummy.StockProduct
 var Customer []dummy.Customer
@@ -122,7 +121,7 @@ func (Controller *Controller) Login(c echo.Context) error {
 	var tmpCustomer dummy.Customer
 	var err error
 	c.Bind(&req)
-	err = Validator.Struct(req)
+	err = validator.New().Struct(req)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"code":     400,
@@ -243,7 +242,7 @@ func (Controller *Controller) Register(c echo.Context) error {
 	var tmpCustomer dummy.Customer
 	var err error
 	c.Bind(&req)
-	err = Validator.Struct(req)
+	err = validator.New().Struct(req)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"code":     400,
